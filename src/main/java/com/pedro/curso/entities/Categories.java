75dc9c783a1d9f59ee46130af1778e32114b4199
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-01-28 19:56:18 -0300

    Dominio de Categories

---
+++ src/main/java/com/pedro/curso/entities/Categories.java
@@ -0,0 +1,53 @@
+package com.pedro.curso.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+//@Entity
+public class Categories implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@Column
+    private String name;
+
+    public Categories(){}
+
+    public Categories(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categories)) return false;
+        Categories that = (Categories) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
--- src/main/java/com/pedro/curso/resources/CategoriesResource.java
+++ src/main/java/com/pedro/curso/resources/CategoriesResource.java
@@ -1,16 +1,28 @@
 package com.pedro.curso.resources;
 
+import com.pedro.curso.entities.Categories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriesResource {
 
     @GetMapping
-    public String test() {
-        return "Hello World";
+    public ResponseEntity<List<Categories>> getCategories() {
+        Categories categorie = new Categories(null, "Banana fish");
+        Categories categorie2 = new Categories(null, "maçã fish");
+
+        List<Categories> categories = new ArrayList<>();
+        categories.add(categorie);
+        categories.add(categorie2);
+
+        return ResponseEntity.ok().body(categories);
     }
 }
